f3b5cfe4a449c99bf91dd6ab85dd5e38ece5f679
Kyle Keen <keenerd@gmail.com>
2012-02-05 17:43:35 -0500

    starting git repo because this is hilariously broken

---
+++ distill.py
@@ -0,0 +1,240 @@
+#! /usr/bin/env python2
+
+# pil on py3 is buggy, won't open png
+
+from PIL import Image
+from itertools import *
+
+# wikireader huge image distill
+# turns an image into a wrhi file
+
+"""
+Convert 1 bit png to a quad tree
+Specifically for the Wikireader
+(Later, make app for panning/zooming these)
+
+Tree should be heap-ish, for easy parsing
+Nodes should have color ratio, for future easy dithering
+
+Node format:
+    color ratio (root has node size instead) (16 bit)
+    is leaf?  (1 bit) pointer (31 bit)
+    is leaf?  (1 bit) pointer (31 bit)
+    is leaf?  (1 bit) pointer (31 bit)
+    is leaf?  (1 bit) pointer (31 bit)
+
+Leaf format:
+    12x12 1 bit block (144 bits)
+
+pointers are always positive
+if leaf and node are same size, scale pointer
+design format with relative scaled pointers in mind
+pointer 0x0000 is out-of-bounding-box child
+pointer 0xFFFF is solid black child
+pointer 0xFFFE is solid white child
+
+Node:
+    ratio/height (4 bit)
+    is leaf? (1 bit) pointer (14 bit)
+    is leaf? (1 bit) pointer (14 bit)
+    is leaf? (1 bit) pointer (14 bit)
+    is leaf? (1 bit) pointer (14 bit)
+    (note that max leap is 16k blocks)
+
+Leaf:
+    8x8 1 bit block (64 bits)
+
+14 bit pointers are just not enough
+since the tree is BF, all quads are sequential
+only store pointer to quad[0]!
+
+Node:
+    ratio/height (8 bit)
+    type 0,1,2,3 (16 bit)
+        exists, leaf, bounds, color
+    child[0] pointer (32 bit)
+    null (8 bit)  # todo - store the edge crop here for not % 8 sizes
+
+Leaf:
+    8x8 1 bit block (64 bits)
+
+Load tree DFS with a stack
+Discard branches that are outside viewport
+If you are clever, panning viewport is only new pixels on edge
+
+"""
+
+class Node(object):
+    def __init__(self, pix, xr, yr):
+        "size is log2"
+        self.root = False
+        self.pix = pix
+        self.xr = xr
+        self.yr = yr
+        self.children = [None] * 4
+        self.out_of_bounds = xr[0]>xr[1] or yr[0]>yr[1]
+        if self.out_of_bounds:
+            return
+        b,w = 0,0
+        # this could be 15 times faster by recursing up instead of down
+        for x,y in product(range(*xr), range(*yr)):
+            try:
+                if pix[x,y]:
+                    b += 1
+                else:
+                    w += 1
+            except IndexError:
+                pass
+        self.size = int_log2(max(xr[1]-xr[0], yr[1]-yr[0]))
+        self.b_count = b
+        self.w_count = w
+    def __repr__(self):
+        return str((self.xr, self.yr, self.children))
+    def bitwise(self, blocks):
+        binary = []
+        if self.root:
+            binary.append(self.size)
+        else:
+            b = self.b_count
+            w = self.w_count
+            ratio = 256.0 * b / (b + w)
+            ratio = int(ratio + 0.5)
+            binary.append(ratio)
+        t0 = block_type(blocks, self.children[0])
+        t1 = block_type(blocks, self.children[1])
+        t2 = block_type(blocks, self.children[2])
+        t3 = block_type(blocks, self.children[3])
+        binary.append(t0*16 + t1)
+        binary.append(t2*16 + t3)
+        # +2 offset for header bytes
+        try:
+            pointer = min(c for c in self.children if type(c) == int) + 2
+        except TypeError:   # no child pointers
+            pointer = 0
+        except ValueError:  # no child pointers
+            pointer = 0
+        assert pointer < len(blocks)
+        binary.extend(bitwise(pointer, 32))
+        if self.root:
+            binary.append((self.xr[1]%8)*16 + self.yr[1]%8)
+        else:
+            binary.append(0)
+        return binary
+
+def block_type(blocks, child):
+    # exists, leaf, bounds, color
+    exists = type(child) in (int, long)
+    leaf   = exists and type(blocks[child]) in (int, long)
+    bounds = child == 'outside'
+    color  = child == 'black'
+    return exists*8 + leaf*4 + bounds*2 + color
+
+def bitwise(n, bits):
+    for sub in range(0, bits, 8):
+        out = 0
+        for i in list(reversed(range(bits)))[sub:sub+8]:
+            j = 2**i
+            out *= 2
+            if n >= j:
+                n -= j
+                out += 1
+        yield out
+
+def print_bin(blocks, block):
+    binary = []
+    if type(block) in (int, long):
+        for b in bitwise(block, 64):
+            binary.append(b)
+        assert len(binary) == 8
+        return binary
+    binary = block.bitwise(blocks)
+    if len(binary) != 8:
+        print block
+        print binary
+    assert len(binary) == 8
+    return binary
+
+def int_log2(n):
+    return [i for i in range(16) if 2**i >= n][0]
+
+def chop_by_quad(node):
+    x1 = node.xr[0]
+    x2 = x1 + 2**(node.size-1)
+    x3 = node.xr[1]
+    y1 = node.yr[0]
+    y2 = y1 + 2**(node.size-1)
+    y3 = node.yr[1]
+    return [Node(node.pix, (x1,x2), (y1,y2)),
+            Node(node.pix, (x2,x3), (y1,y2)),
+            Node(node.pix, (x2,x3), (y2,y3)),
+            Node(node.pix, (x1,x2), (y2,y3))]
+
+def literal(node):
+    "return 64 bit int, black is 1"
+    if node.size != 3:
+        raise
+    n = 0
+    for x,y in product(range(*node.xr), range(*node.yr)):
+        n *= 2
+        try:
+            if pix[x,y]:
+                n += 1
+        except IndexError:
+            pass
+    return n
+
+img = Image.open('lena.png')
+pix = img.load()
+root_height = int_log2(max(img.size))
+#print root_height
+
+blocks = []
+root = Node(pix, (0,img.size[0]), (0,img.size[1]))
+root.root = True
+blocks.append(root)
+#print blocks
+
+todo = [0]  # blocks to quadify
+
+while todo:
+    now = todo.pop(0)
+    new_quads = chop_by_quad(blocks[now])
+    pre_length = len(blocks)
+    added_blocks = []
+    for i,node in enumerate(new_quads):
+        if node.out_of_bounds:
+            blocks[now].children[i] = 'outside'
+            continue
+        if node.b_count == 0:
+            blocks[now].children[i] = 'white'
+            continue
+        if node.w_count == 0:
+            blocks[now].children[i] = 'black'
+            continue
+        blocks.append(None)
+        address = len(blocks) - 1
+        if node.size > 3:
+            blocks[address] = node
+            todo.append(address)
+            added_blocks.append(address)
+        if node.size == 3:
+            blocks[address] = literal(node)
+        blocks[now].children[i] = address
+   # print blocks[now]
+   # for i in added_blocks:
+   #     print "   ", blocks[i].xr, blocks[i].yr
+
+#print
+#print 'block count', len(blocks)
+
+binary = []
+binary.extend(map(ord, '#!quafit\n'))
+binary.extend(map(ord, '#v0.00\n'))
+[binary.extend(print_bin(blocks, b)) for b in blocks]
+
+#print len(binary)
+f = open('lena.wrhi', 'wb')
+f.write(''.join(map(chr, binary)))
+f.close()
+
+
---
+++ quafit.py
@@ -0,0 +1,131 @@
+#! /usr/bin/env python2
+
+import Tkinter as tk
+from itertools import *
+
+# quad format interpreter for wrhi files
+
+"""
+Node:
+    ratio/height (8 bit)
+    type 0,1,2,3 (16 bit)
+        exists, leaf, bounds, color
+    child[0] pointer (32 bit)
+    null (8 bit)  # todo - store the edge crop here for not % 8 sizes
+
+Leaf:
+    8x8 1 bit block (64 bits)
+"""
+
+def fixed_bin(n, width):
+    bs = []
+    for i in reversed(range(width)):
+        j = 2**i
+        bs.append(n >= j)
+        if n >= j:
+            n -= j
+    assert n == 0
+    return bs
+
+class Node(object):
+    "un-serialized file"
+    def __init__(self, chars):
+        "takes 8 bytes of char"
+        self.literal = map(ord, chars)
+        assert len(self.literal) == 8
+        lit = self.literal
+        self.bits = []
+        [self.bits.extend(fixed_bin(n, 8)) for n in lit]
+        self.ratio = lit[0]
+        self.height = lit[0]
+        self.types = [lit[1]//16, lit[1]%16, lit[2]//16, lit[2]%16]
+        self.pointer = lit[3]*2**24 + lit[4]*2**16 + lit[5]*2**8 + lit[6]
+        self.x_clip = lit[7] // 16
+        self.y_clip = lit[7] % 16
+
+class Block(object):
+    "hold state while walking tree"
+    def __init__(self, node, parent=None, quad=None):
+        self.node = node
+        if parent is None:
+            self.xr = (0, 2**node.height)
+            self.yr = (0, 2**node.height)
+            self.height = node.height
+        else:
+            self.xr, self.yr = quad_chop(parent.xr, parent.yr, quad)
+            self.height = parent.height - 1
+        # normally we'd check the bounding box before continuing
+        self.children = [None, None, None, None]
+        if self.height >= 3:
+            self.find_children()
+    def find_children(self):
+        j = 0
+        for i,t in enumerate(self.node.types):
+            exists,leaf,bounds,color = fixed_bin(t, 4)
+            if bounds:
+                self.children[i] = 'outside'
+                continue
+            if exists:
+                self.children[i] = self.node.pointer + j
+                j += 1
+                continue
+            self.children[i] = ('white', 'black')[color]
+
+def quad_chop(xr, yr, quad): 
+    x1,x3 = xr
+    x2 = (x1+x3) // 2
+    y1,y3 = yr
+    y2 = (y1+y3) // 2
+    xr_new = [(x1,x2), (x2,x3), (x2,x3), (x1,x2)][quad]
+    yr_new = [(y1,y2), (y1,y2), (y2,y3), (y2,y3)][quad]
+    return xr_new, yr_new
+
+def draw(canvas, block, quad):
+    if block.children[quad] == 'outside':
+        return
+    if block.children[quad] == 'black':
+        xr,yr = quad_chop(block.xr, block.yr, quad)
+        canvas.create_rectangle(xr[0], yr[0], xr[1], yr[1], fill='black')
+        return
+    if block.height == 3 and quad == 0:  # no need to draw four times
+        # blit the literal
+        xr,yr = block.xr, block.yr
+        points = product(range(xr[0], xr[0]+8), range(yr[0], yr[0]+8))
+        for xy,b in zip(points, block.node.bits):
+            if not b:
+                continue
+            canvas.create_rectangle(xy[0], xy[1], xy[0], xy[1], fill='black')
+
+def load(path):
+    nodes = []
+    string = open(path).read()
+    string = string
+    for i in range(0, len(string), 8):
+        chars = string[i:i+8]
+        nodes.append(Node(chars))
+    print len(nodes)
+    return nodes
+
+def walk(canvas, ns):
+    stack = [Block(ns[2])]
+    while stack:
+        now = stack.pop(0)
+        for q,child in enumerate(now.children):
+            if type(child) in (int, long):
+                try:
+                    stack.append(Block(ns[child], now, q))
+                except IndexError:
+                    print 'block missing', child
+            draw(canvas, now, q)
+
+def main(path):
+    nodes = load(path)
+    root = tk.Tk()
+    canvas = tk.Canvas(root, bg='white')
+    canvas.pack(expand=1, fill=tk.BOTH)
+    canvas.tk_focusFollowsMouse()
+    walk(canvas, nodes)
+    root.mainloop()
+
+main('lena.wrhi')
+
